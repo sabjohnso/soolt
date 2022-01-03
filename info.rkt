3517a2df74d7a571feb46b3cb017b9016db4c4ec
Samuel B. Johnson <sabjohnso@yahoo.com>
2022-01-03 06:57:29 -0600

    Added tag as a dependency

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "soolt")
-(define deps '("base"))
+(define deps '("base" "https://github.com/sabjohnso/tag.git"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/soolt.scrbl" ())))
 (define pkg-desc "Exactly what you'd expect!")
